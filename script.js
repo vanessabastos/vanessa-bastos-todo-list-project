c025b4c1dab54e3afd032ac57d57ba0af030c35e
vanessabastos <vanessarsb83@gmail.com>
2021-07-16 20:55:07 -0300

    enviando desafio 6

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-const btnCriarTarefa = document.getElementById("criar-tarefa");
-const listaTarefas = document.getElementById("lista-tarefas");
-const inputTarefa = document.getElementById("texto-tarefa");
-const btnclear = document.getElementById("apaga-tudo");
+// function createListItem() {
+//   const listItem = document.createElement('li');
+//   listItem.className = 'tarefas';
+//   listItem.innerText = '';
 
+//   return listItem;
+// }
 
-btnCriarTarefa.addEventListener('click',adicionaTarefa);
+// function createList() {
+//   //<ol id="lista-tarefas">
+//   //<li class= "tarefas"></li>
+//   //</ol>
+//   const list = document.getElementById('tarefas');
+//   const listItem = createListItem();
 
-function adicionaTarefa(event) {
-  const textoLista = inputTarefa.value;
-  const itensLista = document.createElement("li");
-  itensLista.innerText = textoLista;
-  itensLista.classList.add("texto-item-list");
-  listaTarefas.appendChild(itensLista);
-  inputTarefa.value = '';
+//   list.appendChild(listItem);
+// }
+
+// function handleClick() {
+//   const list = document.getElementById('tarefas');
+//   const listItem = createListItem();
+// }
+
+// function addEventToButton() {
+//   const button = document.getElementById('criar-tarefa');
+
+//   button.addEventListener('click'. handleClick);
+// }
+// window.onload = function() {
+//   createList();
+//   addEventToButton();
+// }
+
+const inputTarefa = document.getElementById('texto-tarefa');
+const listaOl = document.getElementById('lista-tarefas');
+const btnCriaTarefa = document.getElementById('criar-tarefa');
+
+btnCriaTarefa.addEventListener('click', criarTarefa);
+
+function criarTarefa() {
+    const textoDaLista = inputTarefa.value;
+    const itensLista = document.createElement('li');
+    itensLista.innerText = textoDaLista;
+    listaOl.appendChild(itensLista);
+    itensLista.classList.add('adiciona-item');
+    inputTarefa.value = '';
 }
-
-
-
-
-
-
-
 
 
